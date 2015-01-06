4d5e562ea8336a1aefaf579ae193a842f9ccdf31
Daniel Krueger <keenbug@gmail.com>
2015-01-06 12:50:32 +0100

    added more tangent debugging tools

--- Graphics/Declarative/Cairo/Debug.hs
+++ Graphics/Declarative/Cairo/Debug.hs
@@ -1,7 +1,8 @@
 module Graphics.Declarative.Cairo.Debug where
 
-import           Graphics.Declarative.Bordered  as Bordered
+import           Graphics.Declarative.Bordered  as Bordered  hiding (map)
 import qualified Graphics.Declarative.Border    as Border
+import           Graphics.Declarative.Border (Border)
 
 import           Graphics.Declarative.Cairo.Shape as Shape
 import           Graphics.Declarative.Cairo.Form  as Form
@@ -45,8 +46,27 @@ tangentPath size tangentPoint
 vectorPath :: Vec2 -> Path
 vectorPath vector = pathPoint (0,0) `lineConnect` pathPoint vector
 
+vectorsInCircle :: Double -> [Vec2]
+vectorsInCircle step = map getVector [0,step..2*pi]
+  where
+    getVector dir = Vec2.rotateBy dir Vec2.right
+
+tangentHullPoints :: Double -> Border -> [Vec2]
+tangentHullPoints step border
+  = map (Border.borderOffset border) $ vectorsInCircle step
+
+tangentHullPath :: Double -> Border -> Path
+tangentHullPath step = foldr1 lineConnect . map pathPoint . tangentHullPoints step
+
 debugTangent :: Vec2 -> Form -> Form
 debugTangent direction form = tangent `atop` form
   where
     tangent = outlined (solid red) $ noBorder . openPath $ tangentPath 100 borderVector
     borderVector = Border.borderOffset (Bordered.getBorder form) direction
+
+debugTangentHull :: Double -> Form -> Form
+debugTangentHull step form = hull `atop` form
+  where
+    hull      = outlined (solid red) hullshape
+    hullshape = noBorder . closedPath $ hullpath
+    hullpath  = tangentHullPath step (Bordered.getBorder form)
