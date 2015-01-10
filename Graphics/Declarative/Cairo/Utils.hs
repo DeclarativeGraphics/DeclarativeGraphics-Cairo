0bec883e573f5d7bdb2d96b55fd424cffa5c00d8
Philipp Krüger <matheus.dev@gmail.com>
2015-01-10 10:07:29 +0100

    Actually fix the Utils package...

--- Graphics/Declarative/Cairo/Utils.hs
+++ Graphics/Declarative/Cairo/Utils.hs
@@ -1,8 +1,9 @@
 module Graphics.Declarative.Cairo.Utils where
 
+import Graphics.Rendering.Cairo
 import Graphics.Declarative.Cairo.Form
 
-writeAsSvgToFile :: Int -> Int -> Form -> FilePath -> IO ()
+writeAsSvgToFile :: Double -> Double -> Form -> FilePath -> IO ()
 writeAsSvgToFile width height form file
-  = withSVGSurface path width height $ \ surface ->
+  = withSVGSurface file width height $ \ surface ->
       renderWith surface $ drawForm form
